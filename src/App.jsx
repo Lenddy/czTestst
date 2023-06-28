4cc8a93f5524f7c16cead1f831d3a598f07c77b9
Lenddy <leme2003@gmail.com>
2023-06-27 22:37:01 -0400

    view one

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,3 @@ function App() {
 }
 
 export default App;
-
-// 1 Lincoln free the laves
-
-// 2 Lincoln was president during the civil war
-
-//3 Washington is the father of our country //! ==> Washington
-
-// 4 American Indians lived here first
-
-// 5 Delaware was the first state
-
-// 6 Washingtong was the first president //! ==> Washington
-
-// 7 Adams was the second president
-
-//8 The American Flag is red, white and blue
-
-//9 People want the right to vote
-
-//10 We vote for precident in November  //! ==> president
--- src/components/All_questions_in_order.jsx
+++ src/components/All_questions_in_order.jsx
@@ -1,27 +1,73 @@
+import { useState } from "react";
 import civicQuestions from "./QA_Object";
 
 const All_questions_in_order = () => {
+	const [showAll, setAllShow] = useState(false);
+	const [answerState, setAnswerState] = useState({});
+
+	const show_All_Answer = () => {
+		setAllShow(!showAll);
+	};
+
+	const show_One_Answer = (id) => {
+		setAnswerState((prevState) => ({
+			...prevState,
+			[id]: !prevState[id],
+		}));
+	};
+
 	return (
 		<>
+			<button onClick={show_All_Answer}>
+				{showAll ? "Hide all Answer/s" : "Show all Answer/s"}
+			</button>
+
 			{civicQuestions.map((c) => {
+				const isAwnserVisible = showAll || answerState[c.id]; // change to isAwnserVisible  and   answerState[]
+
 				return (
 					<div key={c.id}>
 						<h1>
 							{c.id}: {c.question}
 						</h1>
-						<div>
-							{Array.isArray(c.answer) ? (
-								<ol>
-									{c.answer.map((a, idx) => {
-										return <li key={idx}>{a}</li>;
-									})}
-								</ol>
-							) : (
-								<ul>
-									<li>{c.answer}</li>
-								</ul>
-							)}
-						</div>
+						{isAwnserVisible && (
+							<div>
+								{Array.isArray(c.answer) ? (
+									<ol>
+										{c.answer.map((a, idx) => {
+											return (
+												<li key={idx}>
+													<h2>{a}</h2>
+												</li>
+											);
+										})}
+									</ol>
+								) : (
+									<ul>
+										<li>
+											<h2>{c.answer}</h2>
+										</li>
+									</ul>
+								)}
+							</div>
+						)}
+						<button
+							style={{ backgroundColor: "purple" }}
+							onClick={() => show_One_Answer(c.id)}
+						>
+							{answerState[c.id]
+								? "Hide Answer/s"
+								: "Show Answer/s"}
+						</button>
+						<button style={{ backgroundColor: "red" }}>
+							failed
+						</button>
+						<button style={{ backgroundColor: "green" }}>
+							pass
+						</button>
+						<button style={{ backgroundColor: "blue" }}>
+							not sure
+						</button>
 					</div>
 				);
 			})}
